83711ec3e3a3f408c130581c3031619fff077130
Cesar Alcancio <cesar.alcancio@gmail.com>
2021-04-29 18:03:52 -0500

    Fim curso de datomic

--- src/cursoseis/aula4.clj
+++ src/cursoseis/aula4.clj
@@ -18,4 +18,35 @@
 (db.config/cria-schema! conn)
 (db.config/cria-dados-de-exemplo! conn)
 
+(defn propriedades-do-valor [valor]
+  (cond (= valor java.util.UUID) {:db/valueType :db.type/uuid
+                                  :db/unique    :db.unique/identity}
+        (= valor s/Str) {:db/valueType :db.type/string}
+        (= valor BigDecimal) {:db/valueType :db.type/bigdec}
+        (= valor Long) {:db/valueType :db.type/long}
+        (= valor s/Bool) {:db/valueType :db.type/boolean}
+        (vector? valor) (merge {:db/cardinality :db.cardinality/many} (propriedades-do-valor (first valor)))
+        (map? valor) {:db/valueType :db.type/ref}
+        :else {:db/valueType (str "unknown: " valor " and type: " (type valor))}))
+
+(defn nome-chave [chave]
+  (cond (keyword? chave) chave
+        (instance? schema.core.OptionalKey chave) (get chave :k)
+        :else (str "unknown: " chave " and type: " (type chave))))
+
+(defn chave-valor-para-definicao [[chave valor]]
+  (let [base {:db/ident       (nome-chave chave)
+              :db/cardinality :db.cardinality/one}
+        extra (propriedades-do-valor valor)
+        schema-do-datomic (merge base extra)]
+    schema-do-datomic))
+
+(defn schema-to-datomic [definicao]
+  (mapv chave-valor-para-definicao definicao)
+  )
+
+;(schema-to-datomic model/Categoria)
+;(schema-to-datomic model/Variacao)
+(pprint (schema-to-datomic model/Produto))
+
 (println "Aula 4")
